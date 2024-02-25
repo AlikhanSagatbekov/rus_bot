aed1cd0314099869edb4da976ada90a525da8d12
AlikhanSagatbekov <221112@astana.edu.kz>
2024-02-25 22:18:13 +0600

    reborn

--- .vs/bot.py
+++ .vs/bot.py
@@ -1,37 +1,9 @@
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telebot
+from telebot import types
 
-# Обработчик команды /start
-def start(update, context):
-    update.message.reply_text(
-        'Привет! Я бот.',
-        reply_markup=ReplyKeyboardMarkup([['Достопримечательности'], ['Топ 10 достопримечательностей'], ['Советы по безопасности и путешествиям']]))
-        
-# Обработчик нажатий на кнопки меню
-def menu_button(update, context):
-    text = update.message.text
-    if text == 'Достопримечательности':
-        update.message.reply_text('Здесь будут перечислены достопримечательности.')
-    elif text == 'Топ 10 достопримечательностей':
-        update.message.reply_text('Здесь будет список топ 10 достопримечательностей.')
-    elif text == 'Советы по безопасности и путешествиям':
-        update.message.reply_text('Здесь будут советы по безопасности и путешествиям.')
-
-def main():
-    # Токен вашего бота
-    token = '6991834091:AAGSERCH4qFfMR18fa2GAIcCxRH9I31rPUQ'
-    updater = Updater(token, use_context=True)
-    dp = updater.dispatcher
-
-    # Добавляем обработчик команды /start
-    dp.add_handler(CommandHandler("start", start))
-    
-    # Добавляем обработчик сообщений с кнопками меню
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, menu_button))
-
-    # Запускаем бота
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+# Токен бота
+BOT_TOKEN = '6991834091:AAGSERCH4qFfMR18fa2GAIcCxRH9I31rPUQ'
+user_states = {}
+sleepTime = 0.5
+# Создание бота
+bot = telebot.TeleBot(BOT_TOKEN)
