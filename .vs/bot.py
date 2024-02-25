4d30280e583ee59182a6ba6ed381257b8c869133
AlikhanSagatbekov <221112@astana.edu.kz>
2024-02-25 21:19:57 +0600

    first commit

---
+++ .vs/bot.py
@@ -0,0 +1,21 @@
+from telegram.ext import Updater, CommandHandler
+
+# Обработчик команды /start
+def start(update, context):
+    update.message.reply_text('Привет! Я бот.')
+
+def main():
+    # Токен вашего бота
+    token = '6991834091:AAGSERCH4qFfMR18fa2GAIcCxRH9I31rPUQ'
+    updater = Updater(token, use_context=True)
+    dp = updater.dispatcher
+
+    # Добавляем обработчик команды /start
+    dp.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
